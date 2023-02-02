38e738831f162e3804b5a87c00f7b92a054da561
JoaoMlnC <joaomlncampos@hotmail.com>
2023-02-01 23:22:37 -0300

    commit operadores, parte 1 a 4

---
+++ tipos-variaveis/src/com/joao/operadores/Operadores2.java
@@ -0,0 +1,13 @@
+package com.joao.operadores;
+
+public class Operadores2 {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        // numero = numero +1;
+        numero++;
+
+        System.out.println(numero);
+
+    }
+}
---
+++ tipos-variaveis/src/com/joao/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,19 @@
+package com.joao.tiposvariaveis;
+
+public class TiposVariaveis {
+  public static void main(String[] args) throws Exception {
+
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 1;
+
+    numero = 2;
+
+    final int VALOR_NUNCA_PODERA_MUDAR = 2;
+
+  }
+}
